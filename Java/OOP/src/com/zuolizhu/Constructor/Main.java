500f6e0ff21c3d485f5076a8e3d256dc038f1d78
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-14 10:06:20 -0400

    Constructor
    Creating object without writing object.setSomething many times.

---
+++ Java/OOP/src/com/zuolizhu/Constructor/Main.java
@@ -0,0 +1,9 @@
+package com.zuolizhu.Constructor;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount imPoor = new BankAccount();
+        imPoor.deposit(50.0);
+        imPoor.withdrawal(20.0);
+    }
+}
