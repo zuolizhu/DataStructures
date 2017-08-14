9712bbbaad477cf5f5b73d871647d07b394d13bf
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-14 10:08:38 -0400

    how to use constructor

--- Java/OOP/src/com/zuolizhu/Constructor/Main.java
+++ Java/OOP/src/com/zuolizhu/Constructor/Main.java
@@ -2,7 +2,7 @@ package com.zuolizhu.Constructor;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount imPoor = new BankAccount();
+        BankAccount imPoor = new BankAccount("1234-5432-1111", 215000, "ZZ","zzhu9@albany.edu", "(000)-000-1234");
         imPoor.deposit(50.0);
         imPoor.withdrawal(20.0);
     }
