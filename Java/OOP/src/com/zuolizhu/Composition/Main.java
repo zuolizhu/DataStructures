002344914484409b4c8996d288451267dfd8f335
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-14 20:34:11 -0400

    composition powerful example

--- Java/OOP/src/com/zuolizhu/Composition/Main.java
+++ Java/OOP/src/com/zuolizhu/Composition/Main.java
@@ -8,8 +8,22 @@ public class Main {
         Motherboard motherboard = new Motherboard("Z270","ASUS", 4,6, "v2.33");
 
         PC nicePC = new PC(theCase, monitor, motherboard);
-        nicePC.getMonitor().drawPixelAt(1500,1200, "yellow");
-        nicePC.getMotherboard().loadProgram("Ubuntu 16.04");
-        nicePC.getTheCase().pressPowerButton();
+        nicePC.powerUp();
+
+
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, "lightBlue");
+        Bed bed = new Bed("Modern", 4,3,2,1);
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("NiceRoom", wall1,wall2,wall3,wall4,ceiling,bed,lamp);
+
+        bedroom.makeBed();
+
+        bedroom.getLamp().turnOn();
     }
 }
