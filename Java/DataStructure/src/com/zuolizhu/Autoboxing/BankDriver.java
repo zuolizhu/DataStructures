109f5740f7a90cfec60ccd3731d1b13b686dfded
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-27 13:17:15 -0400

    test code added

--- Java/DataStructure/src/com/zuolizhu/Autoboxing/BankDriver.java
+++ Java/DataStructure/src/com/zuolizhu/Autoboxing/BankDriver.java
@@ -1,4 +1,32 @@
 package com.zuolizhu.Autoboxing;
 
 public class BankDriver {
+    public static void main(String[] args) {
+
+        Bank bank = new Bank("Chase Bank");
+
+        bank.addBranch("Bayside");
+
+        bank.addCustomer("Bayside", "ZZ", 125000);
+        bank.addCustomer("Bayside", "Irene", 162000);
+
+        bank.addBranch("GreatNeck");
+        bank.addCustomer("GreatNeck", "Popcorn", 100);
+
+        bank.addCustomerTransaction("Bayside", "ZZ", 500);
+        bank.addCustomerTransaction("Bayside", "ZZ", 125);
+        bank.addCustomerTransaction("GreatNeck", "Popcorn", 250000);
+
+        bank.listCustomers("GreatNeck", true);
+        bank.listCustomers("Bayside", true);
+
+        bank.addBranch("Port Washington");
+        if(!bank.addCustomer("Port Washington", "Ben", 70000)) {
+            System.out.println("Branch does not exist");
+        }
+
+        if(!bank.addBranch("Bayside")) {
+            System.out.println("Branch already exists");
+        }
+    }
 }
