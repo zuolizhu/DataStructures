606c7f3ca22931b35e82dd2f726f6caacc038e8b
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-27 13:36:44 -0400

    how ArrayList waste time in inserting

--- Java/DataStructure/src/com/zuolizhu/LinkedList/Main.java
+++ Java/DataStructure/src/com/zuolizhu/LinkedList/Main.java
@@ -1,5 +1,7 @@
 package com.zuolizhu.LinkedList;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer("ZZ", 25);
@@ -11,5 +13,28 @@ public class Main {
          */
         anotherCustomer.setBalance(12.22);
         System.out.println("Balance of customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        intList.add(3);
+        intList.add(5);
+        intList.add(7);
+        intList.add(9);
+
+        for(int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        /**
+         * in Arraylist, if insert an element into the list, the rest element in
+         * this list have to move down space for the new insert one.
+         *
+         * It wastes time
+         */
+        intList.add(1,2);
+
+        for(int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
     }
 }
