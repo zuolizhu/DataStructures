d686bcb21d1ab702572a1378d29345d299600329
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-08-29 23:33:12 -0400

    simple method added

--- Java/DataStructure/src/com/zuolizhu/Interfaces/DeskPhone.java
+++ Java/DataStructure/src/com/zuolizhu/Interfaces/DeskPhone.java
@@ -7,26 +7,35 @@ public class DeskPhone implements ITelephone {
 
     @Override
     public void powerOn() {
-        
+        System.out.println("No action taken, desk phone does not have power button");
     }
 
     @Override
     public void dial(int phoneNumber) {
-
+        System.out.println("Now ringing " + phoneNumber + " on deskphone.");
     }
 
     @Override
     public void answer() {
-
+        if(isRinging) {
+            System.out.println("Answering the desk phone");
+            isRinging = false;
+        }
     }
 
     @Override
     public boolean callPhone(int phoneNumber) {
-        return false;
+        if(phoneNumber == myNumber) {
+            isRinging = true;
+            System.out.println("Ring ring");
+        } else {
+            isRinging = false;
+        }
+        return isRinging;
     }
 
     @Override
     public boolean isRinging() {
-        return false;
+        return isRinging;
     }
 }
