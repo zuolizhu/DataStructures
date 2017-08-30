5c674905cd135b0199c149a9a3117662e261d924
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-08-29 23:38:43 -0400

    test code for ITelephone interface class

--- Java/DataStructure/src/com/zuolizhu/Interfaces/Main.java
+++ Java/DataStructure/src/com/zuolizhu/Interfaces/Main.java
@@ -4,4 +4,11 @@ public class Main {
     /**
      * Interface self is abstract method, there is no actual code to implementation
      */
+    public static void main(String[] args) {
+        ITelephone zzPhone;
+        zzPhone = new DeskPhone(1234567);
+        zzPhone.powerOn();
+        zzPhone.callPhone(666123);
+        zzPhone.answer();
+    }
 }
