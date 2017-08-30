bf65f36349f5b8494da4c1e24205e1a78cb0b271
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-08-29 23:24:00 -0400

    access modifier is not needed

--- Java/DataStructure/src/com/zuolizhu/Interfaces/ITelephone.java
+++ Java/DataStructure/src/com/zuolizhu/Interfaces/ITelephone.java
@@ -5,9 +5,9 @@ public interface ITelephone {
      * The code below is called signature
      * It is not the actual class
      */
-    public void powerOn();
-    public void dial(int phoneNumber);
-    public void answer();
-    public boolean callPhone(int phoneNumber);
-    public boolean isRinging();
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
 }
