a016f3a01af9d0dd3534283e11c4db0939babbe6
zuolizhu <zuolizhu5960x@gmail.com>
2017-09-22 21:34:11 -0400

    customized LinkedList with implementation of interface

---
+++ Java/DataStructure/src/com/zuolizhu/Abstraction/ItemList/Main.java
@@ -0,0 +1,13 @@
+package com.zuolizhu.Abstraction.ItemList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+
+        String[] data = stringData.split(" ");
+        for(String s : data) {
+
+        }
+    }
+}
---
+++ Java/DataStructure/src/com/zuolizhu/Abstraction/ItemList/ZZLinkedList.java
@@ -0,0 +1,72 @@
+package com.zuolizhu.Abstraction.ItemList;
+
+public class ZZLinkedList implements NodeList{
+
+    private ListItem root = null;
+
+    public ZZLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+        if(this.root == null) {
+            /** The list was empty, so this newItem becomes the head of the list
+             */
+            this.root = newItem;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+        while(currentItem != null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if(comparison < 0) {
+                /** newItem is greater, move right if possible
+                 */
+                if(currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    /** There is no next item, insert the newItem at the end of the list
+                     */
+                    currentItem.setNext(newItem);
+                    newItem.setPrevious(currentItem);
+                    return true;
+                }
+            } else if(comparison > 0) {
+                // newItem is less, insert before
+                if(currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem);
+                    newItem.setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem);
+                    currentItem.setPrevious(newItem);
+                } else {
+                    // The node with a previous is the root
+                    newItem.setNext(this.root);
+                    this.root.setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            } else {
+                // Equal
+                System.out.println(newItem.getValue() + " is already added");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}
